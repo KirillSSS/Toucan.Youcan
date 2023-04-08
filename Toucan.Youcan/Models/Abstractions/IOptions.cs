2d12bdfef951e64f1fc2b01842aaf4f5d7928ed9
KirillSSS <savchuk.kirill.ua@gmail.com>
2023-04-08 16:51:20 +0300

    Initial commit

---
+++ Toucan.Youcan/Models/Abstractions/IOptions.cs
@@ -0,0 +1,9 @@
+﻿using Toucan.Youcan.Models.Enums;
+
+namespace Toucan.Youcan.Models.Abstractions
+{
+    public interface IOptions
+    {
+        public List<DateTime> GetAllDays(DateTime startDate, DateTime endDate);
+    }
+}
---
+++ Toucan.Youcan/Models/Subject.cs
@@ -0,0 +1,14 @@
+﻿using Toucan.Youcan.Models.Abstractions;
+using Toucan.Youcan.Models.Enums;
+
+namespace Toucan.Youcan.Models
+{
+    public class Subject : Category
+    {
+        public DateTime StartTime { get; set; }
+
+        public DateTime? EndTime { get; set; }
+
+        public IOptions Frequency { get; set; }
+    }
+}
